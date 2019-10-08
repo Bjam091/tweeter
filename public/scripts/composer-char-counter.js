b7954603f31be3d170380bbd0571a947614db15c
Brandon <brandonjamiller@hotmail.com>
2019-10-08 22:26:02 +0000

    character counter fixed, updates to red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,12 @@ $(document).ready(function() {
   $('.newTweetArea').keyup('keyup' , function() {
     const text = $(this).val();
     const remainCharacters = 140 - text.length
-    const characterUpdate = $(this).siblings('.counter').text(remainCharacters)
-   
+    const characterUpdate = $(this).siblings('#counter').text(remainCharacters)
+    
+
+    if(remainCharacters < 0){
+      document.getElementById('counter').style.color = 'red' ;
+    } 
   
   })
 });
