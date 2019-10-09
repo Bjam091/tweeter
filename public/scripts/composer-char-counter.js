977542a474084593b70b58439b2467723b2fba80
Brandon <brandonjamiller@hotmail.com>
2019-10-09 01:42:42 +0000

    tweet body works, displays correctly with hovering

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,12 +2,16 @@ $(document).ready(function() {
   $('.newTweetArea').keyup('keyup' , function() {
     const text = $(this).val();
     const remainCharacters = 140 - text.length
-    const characterUpdate = $(this).siblings('#counter').text(remainCharacters)
+    $(this).siblings('#counter').text(remainCharacters)
     
+    if(remainCharacters >= 0){
+      document.getElementById('counter').style.color = 'black' ;
+    }
 
     if(remainCharacters < 0){
       document.getElementById('counter').style.color = 'red' ;
     } 
+    
   
   })
 });
